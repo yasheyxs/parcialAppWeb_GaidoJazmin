1a649bc05019391c92c362525be5ef66b19fcb38
yasheyxs <jazmingaidoyxs@gmail.com>
2023-11-09 16:56:46 -0300

    this is a commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const imageGallery = document.getElementById("imageGallery");
+const imageForm = document.getElementById("imageForm");
+
+let images = []; // Almacena las imágenes cargadas
+let idCounter = 1; // ID autoincremental
+
+imageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const imageUrl = document.getElementById("imageUrl").value;
+
+    const newImage = {
+        id: idCounter++,
+        title,
+        description,
+        imageUrl
+    };
+
+    images.push(newImage);
+    displayImages();
+    imageForm.reset(); // Limpia el formulario
+});
+
+function displayImages() {
+    const emptyGalleryMessage = document.getElementById("emptyGalleryMessage");
+
+    imageGallery.innerHTML = "";
+
+    if (images.length === 0) {
+        emptyGalleryMessage.style.display = "block"; // Muestra el mensaje de galería vacía
+    } else {
+        emptyGalleryMessage.style.display = "none"; // Oculta el mensaje de galería vacía
+    }
+
+    images.forEach((image) => {
+        const imageCard = document.createElement("div");
+        imageCard.classList.add("image-card");
+        imageCard.innerHTML = `
+            <img src="${image.imageUrl}" alt="${image.title}">
+            <h3>${image.title}</h3>
+            <p>${image.description}</p>
+            <button onclick="confirmDeleteImage(${image.id})">Eliminar</button> <!-- Agrega confirmación al eliminar -->
+        `;
+        imageGallery.appendChild(imageCard);
+    });
+}
+
+function confirmDeleteImage(id) {
+    const confirmDelete = confirm("¿Estás seguro de que quieres eliminar esta imagen?");
+
+    if (confirmDelete) {
+        deleteImage(id);
+    }
+}
+
+function deleteImage(id) {
+    images = images.filter((image) => image.id !== id);
+    displayImages();
+}
